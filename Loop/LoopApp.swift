f96d1be8d5966d51e67d0ff2a48dc18ff92c5ecc
Eric Rabil <ericjrabil@gmail.com>
2020-07-28 10:16:21 -0400

    Initial Commit

---
+++ Loop/LoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopApp.swift
+//  Loop
+//
+//  Created by Eric Rabil on 7/28/20.
+//
+
+import SwiftUI
+
+@main
+struct LoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
